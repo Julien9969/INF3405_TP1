7656b4d8cbe945d6791d7dfbe230597b41ac10fc
m4rmen <alexandremarmen@gmail.com>
2023-01-23 19:26:32 -0500

    client userInput

--- Server/src/Client.java
+++ Server/src/Client.java
@@ -2,14 +2,30 @@ import java.io.DataInputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
 
 public class Client {
-    
+	public static boolean userCommandPrompt(){
+		System.out.println("Enter command: ");
+		Scanner userCommandInput = new Scanner(System.in);
+		String userStringCommand = userCommandInput.nextLine();
+		String [] splitStr = userStringCommand.split("\\s+");
+		String firstInput = splitStr[0];
+		System.out.println(firstInput);
+		String close = "close";
+		if (firstInput.equals(close)){
+			System.out.println("Closing");
+			return false;
+		}	
+		return true;
+	}
 
 
 private static Socket socket;
 		
 	public static void main(String[] args) throws Exception {
+
 		System.out.println("Entrez l'adresse IP du serveur : ");
 		Scanner userInput = new Scanner(System.in);
 	    String serverAddress = userInput.nextLine();
@@ -19,6 +35,7 @@ private static Socket socket;
 		int port = userInput.nextInt();
 		Verifier.portVerifier(port);
 	    
+		
 		// Création d'une nouvelle connexion aves le serveur
 		socket = new Socket(serverAddress, port);
 		System.out.format("Serveur lancé sur [%s:%d]", serverAddress, port);
@@ -30,6 +47,9 @@ private static Socket socket;
 		String helloMessageFromServer = in.readUTF();
 		System.out.println(helloMessageFromServer);
 		
+		while (userCommandPrompt()){}
+
+
 		// fermeture de La connexion avec le serveur
 		socket.close();
 	}
